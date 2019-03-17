e92ea4c1cf1a35fb626e89e9657a8db85b2ad198
xuyang <914795561@qq.com>
2019-03-17 17:07:55 +0800

    scanner finish

---
+++ src/Url/Scanner.php
@@ -0,0 +1,48 @@
+<?php
+namespace yang\UrlScanner\Url;
+
+use GuzzleHttp\Client;
+
+class Scanner
+{
+    protected $urls;
+    protected $httpClient;
+    public function __construct(array $urls)
+    {
+        $this->urls = $urls;
+        $this->httpClient = new Client();
+    }
+
+    /**
+     * 获取访问这个url的http状态码
+     *
+     * @param [string] $url
+     * @return int
+     */
+    public function getStatusCodeForUrl($url)
+    {
+        $httpResponse = $this->httpClient->get($url);
+        return $httpResponse->getStatusCode();
+    }
+
+    /**
+     * 获取死链
+     *
+     * @return array
+     */
+    public function getInvalidUrls()
+    {
+        $invalidUrls = [];
+        foreach ($this->urls as $url) {
+            try {
+                $statusCode = $this->getStatusCodeForUrl($url);
+            } catch (\Exception $e) {
+                $statusCode = 500;
+            }
+
+            array_push($invalidUrls, ['url' => $url, 'status' => $statusCode]);
+        }
+
+        return $invalidUrls;
+    }
+}
